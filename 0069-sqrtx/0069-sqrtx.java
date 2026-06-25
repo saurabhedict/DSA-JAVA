d3361bd19f3868dee0d5822be04e3c002e17eced
Saurabh Tiwari <saurabhedict@gmail.com>
2026-06-25 15:27:51 +0530

    Time: 1 ms (97.81%), Space: 42.6 MB (41.93%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,40 @@
+// class Solution {
+//     public int mySqrt(int x) {
+//         int lo = 0;
+//         int hi = x;
+//         int mid  = 0;
+
+//         while(lo<=hi){
+//             mid = lo + (hi-lo)/2;
+//             if(mid*mid > x) {
+//                 hi = mid - 1;
+//             }
+//             else if(mid*mid < x) {
+//                 lo = mid + 1;
+//             }
+//             else return mid;
+//         }
+//         return hi;
+//     }
+// } //computation problem
+
+class Solution {
+    public int mySqrt(int x) {
+        if(x <= 1) return x;
+        int lo = 0;
+        int hi = x;
+        int mid  = 0;
+        
+        while(lo<=hi){
+            mid = lo + (hi-lo)/2;
+            if(mid > x/mid) {
+                hi = mid - 1;
+            }
+            else if(mid < x/mid) {
+                lo = mid + 1;
+            }
+            else return mid;
+        }
+        return hi;
+    }
+}
