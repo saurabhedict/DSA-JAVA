8d4a5d67428ba2bdd2590a6bd0efbd53e5aea352
Saurabh Tiwari <saurabhedict@gmail.com>
2026-06-29 13:10:00 +0530

    Time: 0 ms (100%), Space: 44.5 MB (56.49%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,10 +1,34 @@
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         int n = arr.length;
+//         if(n == 1 ) {
+//             if(k>=arr[0]) return k + 1;
+//             else return k;
+//         }
+//         int lo = 0, hi = n-1;
+//         int missing = 0, ans = 0;
+//         while(lo<=hi){
+//             int mid = lo + (hi-lo)/2;
+//             missing = arr[mid]-(mid+1);
+//             if(missing < k) lo = mid + 1;            
+//             else hi = mid - 1;
+//         }
+//         if(hi<0) return k;
+//         else{
+//             int prevMissing = arr[hi] - (hi+1);
+//             int count = k - prevMissing;
+//             ans = arr[hi] + count;
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         int n = arr.length;
-        if(n == 1 ) {
-            if(k>=arr[0]) return k + 1;
-            else return k;
-        }
         int lo = 0, hi = n-1;
         int missing = 0, ans = 0;
         while(lo<=hi){
@@ -13,12 +37,6 @@ class Solution {
             if(missing < k) lo = mid + 1;            
             else hi = mid - 1;
         }
-        if(hi<0) return k;
-        else{
-            int prevMissing = arr[hi] - (hi+1);
-            int count = k - prevMissing;
-            ans = arr[hi] + count;
-        }
-        return ans;
+        return k + lo;
     }
 }
