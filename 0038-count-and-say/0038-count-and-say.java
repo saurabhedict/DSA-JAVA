9376bd94f3c88143167caa00295535aa7baa072a
Saurabh Tiwari <saurabhedict@gmail.com>
2026-06-30 12:23:00 +0530

    Time: 25 ms (5.17%), Space: 52.6 MB (5.07%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String countAndSay(int n) {
+       String s =  "1";
+       if(n == 1) return s;
+       return CAS(n, s, 1);
+    }
+    public String CAS(int n, String s, int x){
+        if(x == n) return s;
+        s = Sb(s);
+        return CAS(n, s, x+1);
+    }
+    public String Sb(String s){
+        StringBuilder st = new StringBuilder(s);
+        int n = s.length();
+        int i = 0;
+        int j = i;
+        String str = "";
+            int count = 0;
+            while(j<n){
+                if(st.charAt(i) == st.charAt(j)){
+                    count++;
+                    j++;
+                }
+                else{
+                  str+=count;
+                  str+=st.charAt(i);
+                  i = j;
+                  count = 0;
+                }
+        }
+         str+=count;
+         str+=st.charAt(i);
+         i = j;
+       return str;
+    }
+    
+}
