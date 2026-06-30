678691efc1c7e5a5c6a7bc44bb571e8da993f7ce
Saurabh Tiwari <saurabhedict@gmail.com>
2026-06-30 12:54:23 +0530

    Time: 24 ms (5.17%), Space: 52.8 MB (5.07%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,3 +1,84 @@
+// class Solution {
+//     public String countAndSay(int n) {
+//        String s =  "1";
+//        if(n == 1) return s;
+//        String str =  CAS(n, s, 1);
+//        return str;
+//     }
+//     public String CAS(int n, String s, int x){
+//         if(x == n) return s;
+//         s = Sb(s);
+//         return CAS(n, s, x+1);
+//     }
+//     public String Sb(String s){
+//         StringBuilder st = new StringBuilder(s);
+//         int n = s.length();
+//         int i = 0;
+//         int j = i;
+//         String str = "";
+//             int count = 0;
+//             while(j<n){
+//                 if(st.charAt(i) == st.charAt(j)){
+//                     count++;
+//                     j++;
+//                 }
+//                 else{
+//                   str+=count;
+//                   str+=st.charAt(i);
+//                   i = j;
+//                   count = 0;
+//                 }
+//         }
+//          str+=count;
+//          str+=st.charAt(i);
+//          i = j;
+//        return str;
+//     }
+    
+// }
+
+
+
+// class Solution {
+//     public String countAndSay(int n) {
+//        String s =  "1";
+//        if(n == 1) return s;
+//        String str =  CAS(n, s, 1);
+//        return str;
+//     }
+//     public String CAS(int n, String s, int x){
+//         if(x == n) return s;
+//         s = Sb(s);
+//         return CAS(n, s, x+1);
+//     }
+//     public String Sb(String s){
+//         StringBuilder st = new StringBuilder(s + "@");
+//         int n = st.length();
+//         int i = 0;
+//         int j = i;
+//         String str = "";
+//             int count = 0;
+//             while(j<n){
+//                 if(st.charAt(i) == st.charAt(j)){
+//                     count++;
+//                     j++;
+//                 }
+//                 else{
+//                   str+=count;
+//                   str+=st.charAt(i);
+//                   i = j;
+//                   count = 0;
+//                 }
+//         }
+//        return str;
+//     }
+    
+// }
+
+
+
+
+
 class Solution {
     public String countAndSay(int n) {
        String s =  "1";
@@ -11,20 +92,20 @@ class Solution {
         return CAS(n, s, x+1);
     }
     public String Sb(String s){
-        StringBuilder st = new StringBuilder(s+"@");
-        int n = st.length();
+         s = s+ "@";
+        int n = s.length();
         int i = 0;
         int j = i;
         String str = "";
             int count = 0;
             while(j<n){
-                if(st.charAt(i) == st.charAt(j)){
+                if(s.charAt(i) == s.charAt(j)){
                     count++;
                     j++;
                 }
                 else{
                   str+=count;
-                  str+=st.charAt(i);
+                  str+=s.charAt(i);
                   i = j;
                   count = 0;
                 }
