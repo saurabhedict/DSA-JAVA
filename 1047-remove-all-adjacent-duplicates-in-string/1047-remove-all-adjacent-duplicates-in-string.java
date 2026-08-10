0bf147ee02357be9e16fe34a904782e027b92f4d
Saurabh Tiwari <saurabhedict@gmail.com>
2026-08-10 16:33:31 +0530

    Time: 129 ms (7.15%), Space: 48.9 MB (8.93%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,10 +1,43 @@
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         int n = 0;
+//         Stack<Character> st = new Stack<>();
+//         Stack<Character> helper = new Stack<>();
+//         String ans = "";
+//         while(n<s.length()){
+//             char ch = s.charAt(n);
+//             if(st.isEmpty()){
+//                 st.push(ch);
+//             }
+//             else if(st.peek() != ch){
+//                 st.push(ch);
+//             }
+//             else {
+//                 st.pop();
+//             }
+//             n++;
+//         }
+
+//         while(!st.isEmpty()){
+//            helper.push(st.pop());
+//         }
+
+//         while(!helper.isEmpty()){
+//             ans += helper.pop();
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
     public String removeDuplicates(String s) {
-        int n = 0;
+        int n = s.length()-1;
         Stack<Character> st = new Stack<>();
-        Stack<Character> helper = new Stack<>();
         String ans = "";
-        while(n<s.length()){
+        while(n>=0){
             char ch = s.charAt(n);
             if(st.isEmpty()){
                 st.push(ch);
@@ -15,15 +48,12 @@ class Solution {
             else {
                 st.pop();
             }
-            n++;
+            n--;
         }
+
 
         while(!st.isEmpty()){
-           helper.push(st.pop());
-        }
-
-        while(!helper.isEmpty()){
-            ans += helper.pop();
+            ans += st.pop();
         }
 
         return ans;
