c6f4684732a3ee48fcfa11577eec357fd9153b2a
Saurabh Tiwari <saurabhedict@gmail.com>
2026-08-16 16:04:16 +0530

    Time: 468 ms (5.24%), Space: 69.9 MB (35.84%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,68 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public boolean isExist(TreeNode root, TreeNode p)
-    {
-       if(root == null) return false; //extra
-       if(root == p) return true;
-       if(root.left == null && root.right == null) return false;
-       if(root.left == p || root.right == p) return true;
-       else
-       {
-          boolean x  = isExist(root.left, p);
-          boolean y =  isExist(root.right, p);
-          if(x || y == true) return true;
-          else return false;
-       }
-    }
+public boolean search(TreeNode root, TreeNode p) {
+    if (root == null) return false;
+    if(root.val == p.val) return true;
 
+    boolean left = search(root.left, p);
+    boolean right = false;
+    if(left == false)  right = search(root.right, p);
+    return (left || right);
+
+    
+}
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        if(root == p || root == q) return root;
-        // if(root.left == null || root.right == null)
-        // {
-        //     if(root.left == null) 
-        //     {
-        //     if(root.right == p && ((root.right.left==q) || (root.right.right==q))) return root.right;
-        //     if(root.right == q && ((root.right.left==p) || (root.right.right==p))) return root.right;
-        //     root = root.right;
-        //     }
-        //     else
-        //     {
-        //     if(root.left == p && ((root.left.left==q) || (root.left.right==q))) return root.left;
-        //     if(root.left == q && ((root.left.left==p) || (root.left.right==p))) return root.left;
-        //     root = root.left;
-        //     }
-        //     return root;
-        // }
-         
-        boolean plef =  isExist(root.left, p);
-        boolean qlef =  isExist(root.left, q);
-
-        if(plef == true && qlef == false)
-        {
-           return root;
-        } 
-        if(plef == true && qlef == true) 
-        {
+        if(root == null) return null;
+        if(p.val == root.val || q.val ==root.val) return root;
+        boolean pleft  = search(root.left, p);
+        boolean qleft = search(root.left, q);
+        if(pleft == true && qleft == true){
             root = root.left;
             return lowestCommonAncestor(root, p, q);
         }
-        if(plef == false && qlef == false)
-        {
+        else if(pleft == false && qleft == false) {
             root = root.right;
             return lowestCommonAncestor(root, p, q);
         }
-
-        
-        return root; //if(plef == false && qlef == true) return root;
-       
+        return root;
     }
 }
