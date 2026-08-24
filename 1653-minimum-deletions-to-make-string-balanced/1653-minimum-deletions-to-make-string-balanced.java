54f60dc465440ead2932a923975362f97e98a9c6
Saurabh Tiwari <saurabhedict@gmail.com>
2026-08-24 23:10:30 +0530

    Time: 43 ms (29.97%), Space: 47.6 MB (64.54%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -55,21 +55,43 @@
 
 
 
+// class Solution {
+//     public int minimumDeletions(String s) {
+//        int n = s.length();
+//        int[] Aafter = new int[n];
+//        int b = 0;
+//        int a = 0;
+//        for(int i = n-1; i>=0; i--){
+//          Aafter[i] = Aafter[i] + a;
+//          if(s.charAt(i) == 'a') a++;
+//        }
+
+//        int min = Integer.MAX_VALUE;
+//        for(int i = 0; i<n; i++){
+//           int sum = Aafter[i] + b;
+//           if(s.charAt(i) == 'b') b++;
+//           min = Math.min(min,sum);
+//        }
+//        return min;
+//     }
+// }
+
+
+
+
 class Solution {
     public int minimumDeletions(String s) {
        int n = s.length();
-       int[] Aafter = new int[n];
        int b = 0;
-
        int a = 0;
-       for(int i = n-1; i>=0; i--){
-         Aafter[i] = Aafter[i] + a;
+       for(int i = 0; i<n; i++){
          if(s.charAt(i) == 'a') a++;
        }
 
        int min = Integer.MAX_VALUE;
        for(int i = 0; i<n; i++){
-          int sum = Aafter[i] + b;
+          if(s.charAt(i) == 'a') a--;
+          int sum = a + b;
           if(s.charAt(i) == 'b') b++;
           min = Math.min(min,sum);
        }
