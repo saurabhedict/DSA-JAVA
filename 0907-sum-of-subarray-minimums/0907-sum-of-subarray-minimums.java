a00dafc2a0d24123c9eee74f97add1901b2a9f49
Saurabh Tiwari <saurabhedict@gmail.com>
2026-08-30 23:26:07 +0530

    Time: 281 ms (51.96%), Space: 103.4 MB (40.2%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public void LeftSmallestElment(int[] arr, int n, int[] LSE){
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i<n; i++){
+         if(st.isEmpty()) LSE[i] = -1;
+         else{
+            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+            st.pop();
+         }
+         if(st.isEmpty()) LSE[i] = -1;
+         else LSE[i] = st.peek();
+         }
+         st.push(i);
+        }
+    }
+    public void RightSmallestElement(int[] arr, int n,int[] RSE){
+        Stack<Integer> st = new Stack<>();
+        for(int i = n-1; i>=0; i--){
+         if(st.isEmpty()) RSE[i] = n;
+        else{
+             while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+            st.pop();
+         }
+         if(st.isEmpty()) RSE[i] = n;
+         else RSE[i] = st.peek();
+         }
+         st.push(i);
+    }
+    }
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int[] LSE = new int[n];
+        int[] RSE = new int[n];
+        
+        LeftSmallestElment(arr, n, LSE);
+        RightSmallestElement(arr, n, RSE);
+
+        long sum1 = 0;
+        for(int i = 0; i<n; i++){
+            int left = i-LSE[i];
+            int right = RSE[i] - i;
+            sum1 = (long)(sum1 + (long)((long)(left*right)*(long)(arr[i])));
+        }
+        long M = (long)Math.pow(10,9) + 7;
+        int sum = (int)(sum1%M);
+
+        return sum;
+    }
+}
