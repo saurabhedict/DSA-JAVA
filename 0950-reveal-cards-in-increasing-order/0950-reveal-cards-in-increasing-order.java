bd56ce3f1a506bcc3d390648f95e4fbf9b0c59c0
Saurabh Tiwari <saurabhedict@gmail.com>
2026-08-31 14:59:03 +0530

    Time: 8 ms (22.75%), Space: 46 MB (31.54%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -1,23 +1,53 @@
+// class Solution {
+//     public int[] deckRevealedIncreasing(int[] deck) {
+//         int n = deck.length;
+//         Arrays.sort(deck);
+//         int[] ans = new int[n];
+
+//         int j = 0; //deck
+//         int i = 0; //ans
+//         boolean mark = false;
+//         while(j<n){
+//            if(ans[i] == 0) {
+//              if(mark == false){
+//                 ans[i] = deck[j];
+//                 j++;
+//              }
+//               mark = !mark;
+//            }
+
+//            i = (i+1)%n;
+//         }
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
         int n = deck.length;
         Arrays.sort(deck);
         int[] ans = new int[n];
-
-        int j = 0; //deck
+        Queue<Integer> q = new LinkedList<>();
         int i = 0; //ans
-        boolean mark = false;
-        while(j<n){
-           if(ans[i] == 0) {
-             if(mark == false){
-                ans[i] = deck[j];
-                j++;
-             }
-              mark = !mark;
-           }
 
-           i = (i+1)%n;
+        for(int k = 0; k<n; k++){
+            q.add(k);
         }
+
+        while(!q.isEmpty()){
+            int idx = q.remove();
+            if(ans[idx] == 0){
+                ans[idx] = deck[i];
+                if(!q.isEmpty()){
+                    int x = q.remove();
+                    q.add(x);
+                }
+
+            }
+             i = (i+1)%n;
+        }
+        
         return ans;
     }
 }
