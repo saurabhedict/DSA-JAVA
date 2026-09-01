49c365458b5fca1c6f7db3552c264d3617a17184
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-01 07:21:59 +0530

    Time: 0 ms (100%), Space: 44.3 MB (30.99%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode Recursive(ListNode temp, ListNode last){
+        if(temp.next == null) {
+            last = temp;
+            return last;
+        }
+        last = Recursive(temp.next, last);
+        temp.next.next = temp;
+        temp.next = null;
+        return last;
+    }
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        return Recursive(head, null);
+    }
+}
