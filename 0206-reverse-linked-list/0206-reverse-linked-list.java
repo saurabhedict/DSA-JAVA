60f6a60d6124f1c66348a0ec9a574819bb29a8ab
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-02 15:03:44 +0530

    Time: 0 ms (100%), Space: 44 MB (84.03%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,35 +1,27 @@
-// class Solution {
-//     public ListNode Recursive(ListNode temp, ListNode last){
-//         if(temp.next == null) {
-//             last = temp;
-//             return last;
-//         }
-//         last = Recursive(temp.next, last);
-//         temp.next.next = temp;
-//         temp.next = null;
-//         return last;
-//     }
-//     public ListNode reverseList(ListNode head) {
-//         if(head == null ||  head.next == null) return head;
-//         return Recursive(head, null);
-//     }
-// } //very imp : revise 
-
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    public ListNode reverseList(ListNode head) {
-
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-
-            ListNode next = curr.next; // save next node
-            curr.next = prev;          // reverse the link
-            prev = curr;               // move prev forward
-            curr = next;               // move curr forward
+    public ListNode reverse(ListNode head, ListNode ans){
+        if(head.next == null){
+            return head;
         }
+        ans = reverse(head.next, ans);
+        head.next.next = head;
+        head.next = null;
+        return ans;
 
-        return prev;
+    }
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+
+        return reverse(head, null);
     }
 }
