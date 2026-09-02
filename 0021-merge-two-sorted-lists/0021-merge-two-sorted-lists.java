b63614594c99ff4c88197cc0e71ace9288d97dc5
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-02 19:35:58 +0530

    Time: 0 ms (100%), Space: 43.9 MB (94.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -23,14 +23,71 @@
 
 
 
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
+//         if(h1 == null) return h2; //optimize
+//         if(h2 == null) return h1; //optimize
+//         ListNode ans = null;
+//         ListNode temp2 = ans;
+        
+//         while(h1 != null || h2 != null){
+
+//             if(h1.val <= h2.val){ //may face null pointer exception
+//              if(ans == null){ 
+//                 ListNode temp = h1;
+//                 h1 = h1.next;
+//                 temp.next = null;
+//                 ans = temp;
+//                 temp2 = ans;
+//              }
+//              else {
+//                 ListNode temp = h1;
+//                 h1 = h1.next;
+//                 temp.next = null;
+//                 temp2.next = temp;
+//                 temp2 = temp2.next;
+//              }
+//              if(h1 == null){
+//                 temp2.next = h2;
+//                 return ans;
+//              }
+//             }
+
+//             else{
+//                 if(ans == null) {
+//                     ListNode temp = h2;
+//                     h2 = h2.next;
+//                     temp.next = null;
+//                     ans = temp;
+//                     temp2 =  ans;
+//                 }
+//                 else {
+//                     ListNode temp = h2;
+//                     h2 = h2.next;
+//                     temp.next = null;
+//                     temp2.next = temp;
+//                     temp2 = temp2.next;
+//                 }
+//                 if(h2 == null){
+//                 temp2.next = h1;
+//                 return ans;
+//              }
+//             }
+//         }
+//         return ans;
+//     }
+// } //worst solution : iterative way
+
+
+
 class Solution {
     public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
-        if(h1 == null) return h2;
-        if(h2 == null) return h1;
+        if(h1 == null) return h2; //optimize
+        if(h2 == null) return h1; //optimize
         ListNode ans = null;
         ListNode temp2 = ans;
         
-        while(h1 != null || h2 != null){
+        while(h1 != null && h2 != null){
 
             if(h1.val <= h2.val){
              if(ans == null){ 
@@ -46,10 +103,6 @@ class Solution {
                 temp.next = null;
                 temp2.next = temp;
                 temp2 = temp2.next;
-             }
-             if(h1 == null){
-                temp2.next = h2;
-                return ans;
              }
             }
 
@@ -68,12 +121,17 @@ class Solution {
                     temp2.next = temp;
                     temp2 = temp2.next;
                 }
-                if(h2 == null){
-                temp2.next = h1;
-                return ans;
              }
             }
-        }
-        return ans;
+
+            if(h1 == null){
+                temp2.next = h2;
+                return ans;
+            }
+            if(h2 == null){
+                temp2.next = h1;
+                return ans;
+            }
+         return ans;
     }
-} //worst solution
+}//worst solution : iterative way
