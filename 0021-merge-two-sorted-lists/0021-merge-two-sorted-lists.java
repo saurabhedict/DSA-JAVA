496522329dcd394f53626844410a3c94b2f42ac0
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-02 19:26:59 +0530

    Time: 0 ms (100%), Space: 44.3 MB (38.74%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,33 +1,79 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
+//         if(h1 == null) {
+//            return h2;
+//         }
+//         if(h2 == null) {
+//             return h1;
+//         }
+//         ListNode ans;
+//         if(h1.val < h2.val){
+//             ans = h1;
+//              ans.next = mergeTwoLists(h1.next, h2);
+//         }
+//         else{
+//             ans = h2;
+//             ans.next = mergeTwoLists(h1, h2.next);
+//         }
+        
+//         return ans;
+//     }
+// }
+
+
 
 
 class Solution {
     public ListNode mergeTwoLists(ListNode h1, ListNode h2) {
-        if(h1 == null) {
-           return h2;
-        }
-        if(h2 == null) {
-            return h1;
-        }
-        ListNode ans;
-        if(h1.val < h2.val){
-            ans = h1;
-             ans.next = mergeTwoLists(h1.next, h2);
-        }
-        else{
-            ans = h2;
-            ans.next = mergeTwoLists(h1, h2.next);
-        }
+        if(h1 == null) return h2;
+        if(h2 == null) return h1;
+        ListNode ans = null;
+        ListNode temp2 = ans;
         
+        while(h1 != null || h2 != null){
+
+            if(h1.val <= h2.val){
+             if(ans == null){ 
+                ListNode temp = h1;
+                h1 = h1.next;
+                temp.next = null;
+                ans = temp;
+                temp2 = ans;
+             }
+             else {
+                ListNode temp = h1;
+                h1 = h1.next;
+                temp.next = null;
+                temp2.next = temp;
+                temp2 = temp2.next;
+             }
+             if(h1 == null){
+                temp2.next = h2;
+                return ans;
+             }
+            }
+
+            else{
+                if(ans == null) {
+                    ListNode temp = h2;
+                    h2 = h2.next;
+                    temp.next = null;
+                    ans = temp;
+                    temp2 =  ans;
+                }
+                else {
+                    ListNode temp = h2;
+                    h2 = h2.next;
+                    temp.next = null;
+                    temp2.next = temp;
+                    temp2 = temp2.next;
+                }
+                if(h2 == null){
+                temp2.next = h1;
+                return ans;
+             }
+            }
+        }
         return ans;
     }
 }
