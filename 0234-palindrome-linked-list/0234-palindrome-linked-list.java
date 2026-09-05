c4342801a20b4d736972363c26646243f23f0ea1
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-06 00:30:36 +0530

    Time: 6 ms (34.37%), Space: 94.6 MB (36.15%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -36,31 +36,85 @@
 // }
 
 
-class Solution {
+
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         int count = 0;
+//         ListNode temp = head;
+//         while(temp != null){
+//             count++;
+//             temp = temp.next;
+//         }
+//         temp = head;
+//         int[] arr = new int[count];
+//         for(int i = 0; i< arr.length; i++){
+//             arr[i] = temp.val;
+//             temp = temp.next;
+//         }
+
+//         int k = 0;
+//         int l = arr.length-1;
+//         while(k <= l){
+//            if(arr[k] != arr[l]) return false;
+//            else{
+//             k++;
+//             l--;
+//            }
+
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+class Solution{
+    public int ListSize(ListNode head){
+       ListNode temp = head;
+       int size = 0;
+       while(temp != null){
+         size++;
+         temp = temp.next;
+       }
+       return size;
+    }
+    public ListNode reverse(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
     public boolean isPalindrome(ListNode head) {
-        int count = 0;
-        ListNode temp = head;
-        while(temp != null){
-            count++;
-            temp = temp.next;
-        }
-        temp = head;
-        int[] arr = new int[count];
-        for(int i = 0; i< arr.length; i++){
-            arr[i] = temp.val;
-            temp = temp.next;
-        }
+       int size = ListSize(head);
+       if(size%2 == 0) size = size/2;
+       else size = (size/2) + 1;
 
-        int k = 0;
-        int l = arr.length-1;
-        while(k <= l){
-           if(arr[k] != arr[l]) return false;
-           else{
-            k++;
-            l--;
-           }
+       ListNode temp = head;
+       for(int i = 1; i<size; i++){
+            temp = temp.next;
+       }
+       ListNode head2 = temp.next;
+       temp.next = null;
 
+       temp = head;
+       ListNode temp2 = head2;
+       temp2 = reverse(head2);
+
+       while(temp != null && temp2 != null){
+        if(temp.val != temp2.val) return false;
+        else{
+            temp = temp.next;
+            temp2 = temp2.next;
         }
-        return true;
+       }
+       return true;
     }
 }
