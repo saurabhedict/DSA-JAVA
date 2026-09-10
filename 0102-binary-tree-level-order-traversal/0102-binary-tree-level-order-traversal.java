13191a586b59335523772cf3b4c4205810b1fa83
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-10 12:38:16 +0530

    Time: 27 ms (1.63%), Space: 46.8 MB (61.36%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,33 +13,122 @@
  *     }
  * }
  */
+// class Solution {
+//     public class pair{
+//         TreeNode node;
+//         int level;
+//         pair(TreeNode root, int level){
+//             this.node = root;
+//             this.level = level;
+//         }
+//     }
+//     public int level(TreeNode root){
+//         if(root == null) return 0;
+//         int left = level(root.left);
+//         int right  = level(root.right);
+//         return 1 + Math.max(left, right);
+//     }
+//     public void fillAnswer(TreeNode root, List<List<Integer>> ans){
+//         Queue<pair> q = new LinkedList<>();
+//         if(root != null) q.add(new pair(root, 0));
+//         while(!q.isEmpty()){
+//             pair front = q.remove();
+//             TreeNode temp = front.node;
+//             int level = front.level;
+//             ans.get(level).add(temp.val);
+//             if(temp.left != null) q.add(new pair(temp.left, level+1));
+//             if(temp.right != null) q.add(new pair(temp.right, level+1));
+
+//         }
+//     }
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         int lvl = level(root);
+//         for(int i = 0; i<lvl; i++){
+//             List<Integer> temp = new ArrayList<>();
+//             ans.add(temp);
+//         }
+//         fillAnswer(root, ans);
+//         return ans;
+
+//     }
+// }
+
+
+// class Solution {
+
+//     public int level(TreeNode root){
+//         if(root == null) return 0;
+//         int left = level(root.left);
+//         int right  = level(root.right);
+//         return 1 + Math.max(left, right);
+//     }
+//     public void fillAnswer(TreeNode root, List<List<Integer>> ans,int i, int x){
+//         if(root == null) return;
+//         if(x == i) ans.get(x).add(root.val);
+//         fillAnswer(root.left, ans, i, x+1);
+//         fillAnswer(root.right, ans,i, x+1);
+//     }
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         int lvl = level(root);
+//         for(int i = 0; i<lvl; i++){
+//             List<Integer> temp = new ArrayList<>();
+//             ans.add(temp);
+//         }
+//         for(int i = 0; i<lvl; i++){
+//             fillAnswer(root, ans, i, 0);
+//         }
+//         return ans;
+
+//     }
+// }
+
+
+
+// class Solution {
+
+//     public int level(TreeNode root){
+//         if(root == null) return 0;
+//         int left = level(root.left);
+//         int right  = level(root.right);
+//         return 1 + Math.max(left, right);
+//     }
+//     public void fillAnswer(TreeNode root, List<List<Integer>> ans,int i, int x){
+//         if(root == null) return;
+//         fillAnswer(root.left, ans, i, x+1);
+//         if(x == i) ans.get(x).add(root.val);
+//         fillAnswer(root.right, ans,i, x+1);
+//     }
+//     public List<List<Integer>> levelOrder(TreeNode root) {
+//         List<List<Integer>> ans = new ArrayList<>();
+//         int lvl = level(root);
+//         for(int i = 0; i<lvl; i++){
+//             List<Integer> temp = new ArrayList<>();
+//             ans.add(temp);
+//         }
+//         for(int i = 0; i<lvl; i++){
+//             fillAnswer(root, ans, i, 0);
+//         }
+//         return ans;
+
+//     }
+// }
+
+
 class Solution {
-    public class pair{
-        TreeNode node;
-        int level;
-        pair(TreeNode root, int level){
-            this.node = root;
-            this.level = level;
-        }
-    }
+
     public int level(TreeNode root){
         if(root == null) return 0;
         int left = level(root.left);
         int right  = level(root.right);
         return 1 + Math.max(left, right);
     }
-    public void fillAnswer(TreeNode root, List<List<Integer>> ans){
-        Queue<pair> q = new LinkedList<>();
-        if(root != null) q.add(new pair(root, 0));
-        while(!q.isEmpty()){
-            pair front = q.remove();
-            TreeNode temp = front.node;
-            int level = front.level;
-            ans.get(level).add(temp.val);
-            if(temp.left != null) q.add(new pair(temp.left, level+1));
-            if(temp.right != null) q.add(new pair(temp.right, level+1));
-
-        }
+    public void fillAnswer(TreeNode root, List<List<Integer>> ans,int i, int x){
+        if(root == null) return;
+        fillAnswer(root.left, ans, i, x+1);
+        fillAnswer(root.right, ans,i, x+1);
+        if(x == i) ans.get(x).add(root.val);
     }
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
@@ -48,7 +137,9 @@ class Solution {
             List<Integer> temp = new ArrayList<>();
             ans.add(temp);
         }
-        fillAnswer(root, ans);
+        for(int i = 0; i<lvl; i++){
+            fillAnswer(root, ans, i, 0);
+        }
         return ans;
 
     }
