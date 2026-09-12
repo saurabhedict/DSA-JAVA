7e1944b75a01200eeaa23f2ecb5bf98d3245f8a1
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-12 14:30:08 +0530

    Time: 1 ms (99.99%), Space: 45.7 MB (10.83%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -17,13 +17,12 @@ class Solution {
     public void PreOrder(TreeNode root, int targetSum, List<List<Integer>> ans, List<Integer> temp){
         if(root == null) return;
         targetSum = targetSum - root.val;
+        temp.add(root.val);
         if(targetSum == 0 && root.left == null && root.right == null){
-           temp.add(root.val);
            ans.add(new ArrayList<>(temp));
            temp.remove(temp.size() - 1);
            return;
         }
-       temp.add(root.val);
        PreOrder(root.left, targetSum, ans, temp);
        PreOrder(root.right, targetSum, ans, temp);
        temp.remove(temp.size() - 1);
