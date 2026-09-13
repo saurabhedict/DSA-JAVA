de1d53d28846b284e2759d43513bbc42c57182ec
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-13 16:19:58 +0530

    Time: 3 ms (30.48%), Space: 45.4 MB (82.99%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,25 +13,53 @@
  *     }
  * }
  */
+
+ 
+// class Solution {
+//     int idx;
+//     public TreeNode solve(int[] preorder, int[] inorder, int start, int end){
+//         if(start > end) return null;
+//         int rootVal = preorder[idx];
+//         int i = start;
+//         for(;i<=end; i++){
+//             if(inorder[i] == rootVal) break;
+//         }
+//         idx++;
+
+//         TreeNode root = new TreeNode(rootVal);
+//         root.left = solve(preorder, inorder,start,i-1);
+//         root.right = solve(preorder, inorder,i+1,end);
+//         return root;
+//     }
+//     public TreeNode buildTree(int[] preorder, int[] inorder) {
+//         int n =  preorder.length;
+//         idx = 0;
+//         return solve(preorder, inorder, 0, n-1);
+//     }
+// }
+
+
+
+
+ 
 class Solution {
     int idx;
-    public TreeNode solve(int[] preorder, int[] inorder, int start, int end){
+    public TreeNode solve(int[] preorder, int[] inorder,int start, int end){
         if(start > end) return null;
         int rootVal = preorder[idx];
+        idx++;
         int i = start;
-        for(;i<=end; i++){
+        for(; i<=end; i++){
             if(inorder[i] == rootVal) break;
         }
-        idx++;
-
         TreeNode root = new TreeNode(rootVal);
-        root.left = solve(preorder, inorder,start,i-1);
-        root.right = solve(preorder, inorder,i+1,end);
+        root.left = solve(preorder, inorder, start, i-1);
+        root.right = solve(preorder, inorder, i+1, end);
         return root;
     }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n =  preorder.length;
-        idx = 0;
-        return solve(preorder, inorder, 0, n-1);
+       int n = preorder.length;
+       idx  = 0;
+       return solve(preorder, inorder, 0, n-1);
     }
 }
