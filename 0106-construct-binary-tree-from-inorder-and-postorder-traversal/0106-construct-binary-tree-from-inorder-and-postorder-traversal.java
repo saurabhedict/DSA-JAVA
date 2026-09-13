3fa394daf1d0b400e1281829ba57c814c00d8419
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-13 21:19:34 +0530

    Time: 3 ms (24.71%), Space: 45.2 MB (92.81%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -39,6 +39,34 @@
 
 
 
+// class Solution {
+//     public TreeNode solve(int[] postorder, int[] inorder, int startIn, int endIn, int startPo, int endPo){
+//         if(startIn > endIn) return null;
+//         if(startPo > endPo) return null;
+//         int rootVal = postorder[endPo];
+//         int i = startIn;
+//         for(;i<=endIn; i++){
+//             if(inorder[i] == rootVal) break;
+//         }
+
+//         // int n = startPost-endPost+1;
+//         int left = i-startIn;
+//         int right = endIn-i;
+
+//         TreeNode root = new TreeNode(rootVal);
+//         root.left = solve(postorder, inorder, startIn, i-1, startPo, startPo+left-1);
+//         root.right = solve(postorder, inorder, i+1, endIn, endPo-right, endPo-1);
+//         return root;
+//     }
+//     public TreeNode buildTree(int[] inorder, int[] postorder) {
+//         int n =  postorder.length;
+//         return solve(postorder,inorder, 0, n-1, 0, n-1);
+//     }
+// }
+
+
+
+
 class Solution {
     public TreeNode solve(int[] postorder, int[] inorder, int startIn, int endIn, int startPo, int endPo){
         if(startIn > endIn) return null;
@@ -55,7 +83,7 @@ class Solution {
 
         TreeNode root = new TreeNode(rootVal);
         root.left = solve(postorder, inorder, startIn, i-1, startPo, startPo+left-1);
-        root.right = solve(postorder, inorder, i+1, endIn, endPo-right, endPo-1);
+        root.right = solve(postorder, inorder, i+1, endIn, startPo+left, endPo-1);
         return root;
     }
     public TreeNode buildTree(int[] inorder, int[] postorder) {
