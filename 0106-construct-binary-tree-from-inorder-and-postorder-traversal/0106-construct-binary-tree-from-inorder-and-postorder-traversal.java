3a3bdcf7b49c47abd872f207524aa209b22382bf
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-13 21:18:56 +0530

    Time: 3 ms (24.71%), Space: 45.6 MB (80.41%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -13,26 +13,53 @@
  *     }
  * }
  */
+// class Solution {
+//      int idx;
+//     public TreeNode solve(int[] postorder, int[] inorder, int start, int end){
+//         if(start > end) return null;
+//         int rootVal = postorder[idx];
+//         int i = start;
+//         for(;i<=end; i++){
+//             if(inorder[i] == rootVal) break;
+//         }
+//         idx--;
+
+
+//         TreeNode root = new TreeNode(rootVal);
+//         root.right = solve(postorder, inorder,i+1, end);
+//         root.left = solve(postorder, inorder,start,i-1);
+//         return root;
+//     }
+//     public TreeNode buildTree(int[] inorder, int[] postorder) {
+//         int n =  postorder.length;
+//         idx = n-1;
+//         return solve(postorder,inorder, 0, n-1);
+//     }
+// }
+
+
+
 class Solution {
-     int idx;
-    public TreeNode solve(int[] postorder, int[] inorder, int start, int end){
-        if(start > end) return null;
-        int rootVal = postorder[idx];
-        int i = start;
-        for(;i<=end; i++){
+    public TreeNode solve(int[] postorder, int[] inorder, int startIn, int endIn, int startPo, int endPo){
+        if(startIn > endIn) return null;
+        if(startPo > endPo) return null;
+        int rootVal = postorder[endPo];
+        int i = startIn;
+        for(;i<=endIn; i++){
             if(inorder[i] == rootVal) break;
         }
-        idx--;
 
+        // int n = startPost-endPost+1;
+        int left = i-startIn;
+        int right = endIn-i;
 
         TreeNode root = new TreeNode(rootVal);
-        root.right = solve(postorder, inorder,i+1, end);
-        root.left = solve(postorder, inorder,start,i-1);
+        root.left = solve(postorder, inorder, startIn, i-1, startPo, startPo+left-1);
+        root.right = solve(postorder, inorder, i+1, endIn, endPo-right, endPo-1);
         return root;
     }
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         int n =  postorder.length;
-        idx = n-1;
-        return solve(postorder,inorder, 0, n-1);
+        return solve(postorder,inorder, 0, n-1, 0, n-1);
     }
 }
