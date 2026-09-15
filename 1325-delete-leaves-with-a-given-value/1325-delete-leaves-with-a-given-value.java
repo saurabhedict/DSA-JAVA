e7aab8ae12bc7b27b40b646cf73f16ef7ec3b6e6
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-15 20:47:23 +0530

    Time: 0 ms (100%), Space: 46.3 MB (68.4%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.java
@@ -14,28 +14,47 @@
  * }
  */
  
+// class Solution {
+//     public boolean solve(TreeNode root, int target){
+//         if(root == null) return false;
+//         if((root.left == null && root.right == null) && root.val == target) return true;
+        
+//         boolean x = solve(root.left, target);
+//         if(x) {
+//             root.left = null;
+//             if(root.right == null && root.val == target) return true;
+//         }
+
+//         boolean y = solve(root.right, target);
+//          if(y) {
+//             root.right = null;
+//             if(root.left == null && root.val == target) return true;
+//         }
+
+//         return false;
+//     }
+//     public TreeNode removeLeafNodes(TreeNode root, int target) {
+//         if((root.left == null && root.right == null) && root.val == target) return null;
+//         boolean ans = solve(root, target);
+//         if(ans) return null;
+//         return root;
+//     }
+// }
+
+
+
+
 class Solution {
-    public boolean solve(TreeNode root, int target){
-        if(root == null) return false;
-        if((root.left == null && root.right == null) && root.val == target) return true;
-        boolean x = solve(root.left, target);
-        if(x) {
-            root.left = null;
-            if(root.right == null && root.val == target) return true;
-        }
-
-        boolean y = solve(root.right, target);
-         if(y) {
-            root.right = null;
-            if(root.left == null && root.val == target) return true;
-        }
-
-        return false;
+    public TreeNode solve(TreeNode root, int target){
+        if(root == null) return null;
+        root.left = solve(root.left, target);
+        root.right = solve(root.right, target);
+        if((root.left == null && root.right == null) && root.val == target) return null;
+        return root;
     }
     public TreeNode removeLeafNodes(TreeNode root, int target) {
         if((root.left == null && root.right == null) && root.val == target) return null;
-        boolean ans = solve(root, target);
-        if(ans) return null;
-        return root;
+        TreeNode ans = solve(root, target);
+        return ans;
     }
 }
