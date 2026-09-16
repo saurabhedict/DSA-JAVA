74c5b9b689cc6fa8b9b07cf85368890ad1203ba0
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-16 21:17:50 +0530

    Time: 1 ms (16.4%), Space: 45.4 MB (15.1%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+
 class Solution {
     public void fillArray(TreeNode root, ArrayList<Integer> list){
         if(root == null) return;
@@ -24,10 +25,9 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
         fillArray(root, list);
         int n = list.size();
-        boolean flag = true;
         for(int i = 1; i<n; i++){
          if(list.get(i) <= list.get(i-1)) return false;
         }
-        return flag;
+        return true;
     }
 }
