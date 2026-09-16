cccc2b3b4c34eddd774779c678b7eb6fbb8cfa5e
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-16 21:53:18 +0530

    Time: 3 ms (2.35%), Space: 45.1 MB (65.98%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,20 +14,46 @@
  * }
  */
 
+// class Solution {
+//     public void fillArray(TreeNode root, ArrayList<Integer> list){
+//         if(root == null) return;
+//         fillArray(root.left, list);
+//         list.add(root.val);
+//         fillArray(root.right, list);
+//     }
+//     public boolean isValidBST(TreeNode root) {
+//         ArrayList<Integer> list = new ArrayList<>();
+//         fillArray(root, list);
+//         int n = list.size();
+//         for(int i = 1; i<n; i++){
+//          if(list.get(i) <= list.get(i-1)) return false;
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
 class Solution {
-    public void fillArray(TreeNode root, ArrayList<Integer> list){
-        if(root == null) return;
-        fillArray(root.left, list);
-        list.add(root.val);
-        fillArray(root.right, list);
+    public long max(TreeNode root){
+      if(root == null) return Long.MIN_VALUE;
+      long current = root.val;
+      long left = max(root.left);
+      long right = max(root.right);
+      return Math.max(current, Math.max(left, right));
+    }
+    public long min(TreeNode root){
+      if(root == null) return Long.MAX_VALUE;
+      long current = root.val;
+      long left = min(root.left);
+      long right = min(root.right);
+      return Math.min(current, Math.min(left, right));
     }
     public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        fillArray(root, list);
-        int n = list.size();
-        for(int i = 1; i<n; i++){
-         if(list.get(i) <= list.get(i-1)) return false;
-        }
-        return true;
+       if(root == null || (root.left == null && root.right == null)) return true;
+       if(root.val <= max(root.left)) return false;
+       if(root.val >= min(root.right)) return false;
+       return isValidBST(root.left) && isValidBST(root.right);
     }
 }
