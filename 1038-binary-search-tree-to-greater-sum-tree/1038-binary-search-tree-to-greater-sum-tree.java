ad4c90a2e70d0bee41556df861d74e5769b2ac82
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-17 19:06:04 +0530

    Time: 0 ms (100%), Space: 42.6 MB (97.14%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -13,14 +13,36 @@
  *     }
  * }
  */
+
+
+// class Solution {
+//     int sum = 0;
+//     public TreeNode bstToGst(TreeNode root) {
+//         if(root == null) return null;
+//         bstToGst(root.right);
+//         sum += root.val;
+//         root.val = sum;
+//         bstToGst(root.left);
+//         return root;
+//     }
+// }
+
+
+
+
 class Solution {
-    int sum = 0;
+    public void fillList(TreeNode root, ArrayList<TreeNode> list){
+        if(root == null) return;
+        fillList(root.left, list);
+        list.add(root);
+        fillList(root.right, list);
+    }
     public TreeNode bstToGst(TreeNode root) {
-        if(root == null) return null;
-        bstToGst(root.right);
-        sum += root.val;
-        root.val = sum;
-        bstToGst(root.left);
-        return root;
+       ArrayList<TreeNode> list = new ArrayList<>();
+       fillList(root, list);
+       for(int i = list.size()-2; i>=0; i--){
+            list.get(i).val += list.get(i+1).val;
+       }
+       return root;
     }
 }
