3c53a033862a863db88ed81f7dbac7509f223b68
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-20 21:09:20 +0530

    Time: 12 ms (96.49%), Space: 55.1 MB (15.06%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -42,7 +42,7 @@
     }
     
     Node[] table;
-    int size = 10000; //you can make any size, the more size : less collision handling  and less searching time.
+    int size = 100000; //you can make any size, the more size : less collision handling  and less searching time.
     //the less size : more collision handling and more searching time
 
     public int hash(int key){
