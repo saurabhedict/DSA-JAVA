bfca290eb1874583de2f1ea0b7230e3f6ac7722e
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-21 00:39:49 +0530

    Time: 19 ms (11.1%), Space: 52.5 MB (65.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,43 @@
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         List<Integer> ans = new ArrayList<>();
+//         if(nums.length == 1) {
+//             ans.add(nums[0]);
+//             return ans;
+//         }
+//         if(nums.length == 2){
+//             if(nums[0] != nums[1]){
+//                 ans.add(nums[0]);
+//                 ans.add(nums[1]);
+//                 return ans;
+//             }
+//         }
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int ele : nums){
+//             if(map.containsKey(ele)){
+//                 int val = map.get(ele);
+//                 val = val+1;
+//                 if(val > nums.length/3) set.add(ele);
+//                 map.put(ele, val);
+//             }
+//            else map.put(ele, 1);
+//         }
+
+//         for (int ele : set) {
+//          ans.add(ele);
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans = new ArrayList<>();
-        if(nums.length == 1) {
-            ans.add(nums[0]);
-            return ans;
-        }
-        if(nums.length == 2){
-            if(nums[0] != nums[1]){
-                ans.add(nums[0]);
-                ans.add(nums[1]);
-                return ans;
-            }
-        }
         HashMap<Integer, Integer> map = new HashMap<>();
         HashSet<Integer> set = new HashSet<>();
 
@@ -22,7 +48,10 @@ class Solution {
                 if(val > nums.length/3) set.add(ele);
                 map.put(ele, val);
             }
-           else map.put(ele, 1);
+           else {
+            map.put(ele, 1);
+            if(1 > nums.length/3) set.add(ele);
+            }
         }
 
         for (int ele : set) {
