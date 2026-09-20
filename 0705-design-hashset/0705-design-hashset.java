387be8abe78a8ba353b460072bbc5f4be010e8e2
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-20 21:05:43 +0530

    Time: 75 ms (10.88%), Space: 49.7 MB (94.65%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,24 +1,24 @@
-class MyHashSet {
-    boolean[] arr = new boolean[1000001];
+// class MyHashSet {
+//     boolean[] arr = new boolean[1000001];
 
-    public MyHashSet() {
+//     public MyHashSet() {
         
-    }
+//     }
     
-    public void add(int key) {
-        arr[key] = true;
-    }
+//     public void add(int key) {
+//         arr[key] = true;
+//     }
     
-    public void remove(int key) {
-        arr[key] = false;
-    }
+//     public void remove(int key) {
+//         arr[key] = false;
+//     }
     
-    public boolean contains(int key) {
-        if(arr[key] == true) return true;
-        else return false;
-    }
-}
-//very very very imp question
+//     public boolean contains(int key) {
+//         if(arr[key] == true) return true;
+//         else return false;
+//     }
+// }
+// //very very very imp question
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet obj = new MyHashSet();
@@ -26,3 +26,93 @@ class MyHashSet {
  * obj.remove(key);
  * boolean param_3 = obj.contains(key);
  */
+
+
+
+
+ class MyHashSet {
+
+    class Node{
+        int val;
+        Node next;
+
+        Node(int val){
+            this.val = val;
+        }
+    }
+    
+    Node[] table;
+    int size = 10;
+
+    public int hash(int key){
+        return key%size;
+    }
+
+    public MyHashSet() {
+        table = new Node[size];
+    }
+
+    public void add(int key) {
+        boolean search = contains(key);
+        if(search) return;
+        
+        int idx = hash(key);
+        if(table[idx] == null){
+            Node temp = new Node(key);
+            table[idx] = temp;
+            return;
+        }
+        
+        Node curr = table[idx];
+
+        while(curr.next != null){
+           curr = curr.next;
+        }
+        Node temp = new Node(key);
+        curr.next = temp;
+    }
+    
+    public void remove(int key) {
+        boolean search = contains(key);
+        if(!search) return;
+
+        int idx = hash(key);
+
+        Node prev = null;
+        Node curr = table[idx];
+
+        while(curr != null){
+
+            if(curr.val == key){
+                
+                //first node to delete
+                if(prev == null){
+                    curr = curr.next;
+                    table[idx] = curr;
+                }
+
+                //middle  node
+                else prev.next = curr.next;
+
+                return;
+            }
+
+            prev = curr;
+            curr = curr.next;
+        }
+    }
+    
+    public boolean contains(int key) {
+        int idx = hash(key);
+        
+        if(table[idx] == null) return false;
+
+        Node curr = table[idx];
+
+        while(curr != null){
+            if(curr.val == key) return true;
+            curr = curr.next;
+        }
+        return false;
+    }
+}
