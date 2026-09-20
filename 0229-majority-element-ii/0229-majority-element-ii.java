59cf412b3e8b9cc56a09979c39ad48435cd1c38e
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-21 00:46:34 +0530

    Time: 17 ms (17.01%), Space: 52.7 MB (29.94%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -35,27 +35,52 @@
 
 
 
+// class Solution {
+//     public List<Integer> majorityElement(int[] nums) {
+//         List<Integer> ans = new ArrayList<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         HashSet<Integer> set = new HashSet<>();
+
+//         for(int ele : nums){
+//             if(map.containsKey(ele)){
+//                 int val = map.get(ele);
+//                 val = val+1;
+//                 if(val > nums.length/3) set.add(ele);
+//                 map.put(ele, val);
+//             }
+//            else {
+//             map.put(ele, 1);
+//             if(1 > nums.length/3) set.add(ele);
+//             } //imp
+//         }
+
+//         for (int ele : set) {
+//          ans.add(ele);
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> ans = new ArrayList<>();
         HashMap<Integer, Integer> map = new HashMap<>();
-        HashSet<Integer> set = new HashSet<>();
 
         for(int ele : nums){
             if(map.containsKey(ele)){
                 int val = map.get(ele);
-                val = val+1;
-                if(val > nums.length/3) set.add(ele);
-                map.put(ele, val);
+                map.put(ele, val+1);
             }
-           else {
-            map.put(ele, 1);
-            if(1 > nums.length/3) set.add(ele);
-            }
+           else map.put(ele, 1);
         }
 
-        for (int ele : set) {
-         ans.add(ele);
+        for(int key : map.keySet()){
+            int val = map.get(key);
+            if(val > nums.length/3) ans.add(key);
         }
         return ans;
     }
