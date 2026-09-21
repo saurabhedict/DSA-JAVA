10ba6e337f9f3f241e956c389ba1b0895ac657de
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-21 19:15:38 +0530

    Time: 42 ms (20.98%), Space: 92.3 MB (30.31%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -27,6 +27,83 @@
 // } //solution is wrong 
 
 
+
+
+// class Solution {
+//     public boolean canArrange(int[] arr, int k) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int ele : arr){
+//             ele = ele%k;
+//             if(ele < 0) ele += k;
+//             if(map.containsKey(ele)){
+//                 int freq = map.get(ele);
+//                 map.put(ele, freq+1);
+//             }
+//             else map.put(ele, 1);
+//         }
+    
+//         if(map.containsKey(0)){
+//             if(map.get(0)%2 != 0) return false;
+//             map.remove(0);
+//         }
+//         // if(k%2 == 0 && map.containsKey(k/2)){
+//         //     if(map.get(k/2)%2 != 0) return false;
+//         //     map.remove(k/2);
+//         // } //we don't need this actually
+
+//         for(int ele : map.keySet()){
+//             int rem = k - ele;
+//             if(!map.containsKey(rem)) return false;
+//             else{
+//                 int freq1 = map.get(ele);
+//                 int freq2 = map.get(rem);
+//                 if(freq1 != freq2) return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
+
+// class Solution {
+//     public boolean canArrange(int[] arr, int k) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int ele : arr){
+//             ele = ele%k;
+//             if(ele < 0) ele += k;
+//             if(map.containsKey(ele)){
+//                 int freq = map.get(ele);
+//                 map.put(ele, freq+1);
+//             }
+//             else map.put(ele, 1);
+//         }
+    
+//         if(map.containsKey(0)){
+//             if(map.get(0)%2 != 0) return false;
+//             map.remove(0);
+//         }
+
+//         for(int ele : map.keySet()){
+//             int rem = k - ele;
+//             if(!map.containsKey(rem)) return false;
+//             else{
+//                 int freq1 = map.get(ele);
+//                 int freq2 = map.get(rem);
+//                 if(freq1 != freq2) return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+
+
+
 class Solution {
     public boolean canArrange(int[] arr, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -41,16 +118,13 @@ class Solution {
             else map.put(ele, 1);
         }
     
-        if(map.containsKey(0)){
-            if(map.get(0)%2 != 0) return false;
-            map.remove(0);
-        }
-        // if(k%2 == 0 && map.containsKey(k/2)){
-        //     if(map.get(k/2)%2 != 0) return false;
-        //     map.remove(k/2);
+        // if(map.containsKey(0)){
+        //     if(map.get(0)%2 != 0) return false;
+        //     map.remove(0);
         // }
 
         for(int ele : map.keySet()){
+            if(ele == 0 && map.get(ele)%2 == 0) continue;
             int rem = k - ele;
             if(!map.containsKey(rem)) return false;
             else{
