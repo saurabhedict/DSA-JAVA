2746393612b5ddde5d0559086c179863b863693d
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-21 19:13:22 +0530

    Time: 43 ms (17.67%), Space: 92 MB (36.44%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,64 @@
+// class Solution {
+//     public boolean canArrange(int[] arr, int k) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+
+//         for(int ele : arr){
+//             ele = ele%k;
+//             if(map.containsKey(ele)){
+//                 int freq = map.get(ele);
+//                 map.put(ele, freq+1);
+//             }
+//             else map.put(ele, 1);
+//         }
+
+//         for(int ele : map.keySet()){
+//             if(ele == 0 && map.get(ele)%2 == 0) continue;
+//             if(ele == 0 && map.get(ele)%2 != 0) return false;
+//             int rem = k - ele;
+//             if(!map.containsKey(rem)) return false;
+//             else{
+//                 int freq1 = map.get(ele);
+//                 int freq2 = map.get(rem);
+//                 if(freq1 != freq2) return false;
+//             }
+//         }
+//         return true;
+//     }
+// } //solution is wrong 
+
+
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int ele : arr){
+            ele = ele%k;
+            if(ele < 0) ele += k;
+            if(map.containsKey(ele)){
+                int freq = map.get(ele);
+                map.put(ele, freq+1);
+            }
+            else map.put(ele, 1);
+        }
+    
+        if(map.containsKey(0)){
+            if(map.get(0)%2 != 0) return false;
+            map.remove(0);
+        }
+        // if(k%2 == 0 && map.containsKey(k/2)){
+        //     if(map.get(k/2)%2 != 0) return false;
+        //     map.remove(k/2);
+        // }
+
+        for(int ele : map.keySet()){
+            int rem = k - ele;
+            if(!map.containsKey(rem)) return false;
+            else{
+                int freq1 = map.get(ele);
+                int freq2 = map.get(rem);
+                if(freq1 != freq2) return false;
+            }
+        }
+        return true;
+    }
+}
