02b4bed78bb382ff8e52b2d71efd2c5fa23afaa5
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-23 10:11:56 +0530

    Time: 37 ms (27.24%), Space: 67.6 MB (94.17%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,24 +1,59 @@
+// class Solution {
+//     public int maxOperations(int[] nums, int k) {
+//         int opt = 0;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int ele : nums){
+//             int rem = k - ele;
+//             if(map.containsKey(rem) || map.containsKey(ele)){
+//               if(map.containsKey(rem)){
+//               int val = map.get(rem);
+//               val = val - 1;
+//               if(val == 0) map.remove(rem);
+//               else map.put(rem, val);
+//               opt++;
+//               }
+//               else if(map.containsKey(ele)){
+//                 int val = map.get(ele);
+//                 map.put(ele, val+1);
+//               }
+//             }
+//             else map.put(ele, 1);
+//         }
+//          return opt;
+//     }
+// }
+
+
+
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int opt = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        for(int ele : nums){
+
+        for (int ele : nums) {
             int rem = k - ele;
-            if(map.containsKey(rem) || map.containsKey(ele)){
-              if(map.containsKey(rem)){
-              int val = map.get(rem);
-              val = val - 1;
-              if(val == 0) map.remove(rem);
-              else map.put(rem, val);
-              opt++;
-              }
-              else if(map.containsKey(ele)){
-                int val = map.get(ele);
-                map.put(ele, val+1);
-              }
+
+            if (map.containsKey(rem)) {
+                int val = map.get(rem);
+
+                if (val == 1)
+                    map.remove(rem);
+                else
+                    map.put(rem, val - 1);
+
+                opt++;
+            } 
+            else {
+                if (map.containsKey(ele)) {
+                    int val = map.get(ele);
+                    map.put(ele, val + 1);
+                } 
+                else {
+                    map.put(ele, 1);
+                }
             }
-            else map.put(ele, 1);
         }
-         return opt;
+
+        return opt;
     }
 }
