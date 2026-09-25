2972d92bc1043d3bee37840750a600cfff42b05d
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-25 09:37:42 +0530

    Time: 51 ms (11.05%), Space: 59.3 MB (15.45%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,3 +1,45 @@
+// class Solution {
+//     public class Pair{
+//         int x;
+//         int y;
+//         Pair(int x, int y){
+//             this.x = x;
+//             this.y = y;
+//         }
+        
+//     }
+//     public int dist(int[] arr){
+//         return arr[0]*arr[0] + arr[1]*arr[1];
+//     }
+//     public int[][] kClosest(int[][] points, int k) {
+//         HashMap<Integer, Pair> map = new HashMap<>();
+//         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+//         int[][] ans = new int[k][2];
+
+//         for(int[] ele : points){
+//             int dist = dist(ele);
+//             map.put(dist, new Pair(ele[0], ele[1]));
+//         }
+        
+//         for(int ele : map.keySet()){
+//             pq.add(ele);
+//             if(pq.size()>k) pq.remove();
+//         }
+
+//         for(int i = 0; i<k; i++){
+//             int num = pq.remove();
+//             ans[i][0] = map.get(num).x;
+//             ans[i][1] = map.get(num).y;
+//         }
+//         return ans;
+//     }
+// }
+
+
+
+
+
+
 class Solution {
     public class Triplet implements Comparable<Triplet>{
         int d;
@@ -27,10 +69,8 @@ class Solution {
         }
         for(int i = 0; i<k; i++){
             Triplet t = pq.remove();
-            int x = t.x;
-            int y = t.y;
-            ans[i][0] = x;
-            ans[i][1] = y;
+            ans[i][0] = t.x;
+            ans[i][1] = t.y;
         }
         return ans;
     }
