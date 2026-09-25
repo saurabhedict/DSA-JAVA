1194bae6d40cec2f1decf35d7ba6b406d13ca965
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-25 11:19:01 +0530

    Time: 5 ms (94.9%), Space: 45.6 MB (64.68%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,3 +1,54 @@
+// class Solution {
+//     public class Pair implements Comparable<Pair>{
+//         int key;
+//         int val;
+//         Pair(int key, int val){
+//             this.key = key;
+//             this.val = val;
+//         }
+//         public int compareTo(Pair p) {
+//             if (this.val != p.val) {
+//                return this.val - p.val;      // smaller frequency first
+//             }
+//             return p.key - this.key;          // bigger key first
+//         }
+//     }
+//     public int[] frequencySort(int[] nums) {
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int ele : nums){
+//             if(map.containsKey(ele)){
+//                 int freq = map.get(ele);
+//                 map.put(ele, freq+1);
+//             }
+//             else map.put(ele, 1);
+//         }
+//         PriorityQueue<Pair> pq = new PriorityQueue<>();
+        
+//         for(int ele : map.keySet()){
+//             pq.add(new Pair(ele, map.get(ele)));
+//         }
+        
+//         int n = pq.size();
+//         int idx = 0;
+//         for(int i = 0; i<n; i++){
+//            Pair p = pq.remove();
+//            int key = p.key;
+//            int freq = p.val;
+//            for(int j = 0; j<freq; j++){
+//                nums[idx] = key;
+//                idx++;
+//            }
+
+//         }
+//         return nums;
+        
+//     }
+// }
+
+
+
+
+
 class Solution {
     public class Pair implements Comparable<Pair>{
         int key;
@@ -10,7 +61,6 @@ class Solution {
             if (this.val != p.val) {
                return this.val - p.val;      // smaller frequency first
             }
-
             return p.key - this.key;          // bigger key first
         }
     }
@@ -29,9 +79,9 @@ class Solution {
             pq.add(new Pair(ele, map.get(ele)));
         }
         
-        int n = pq.size();
+
         int idx = 0;
-        for(int i = 0; i<n; i++){
+        while(!pq.isEmpty()){
            Pair p = pq.remove();
            int key = p.key;
            int freq = p.val;
