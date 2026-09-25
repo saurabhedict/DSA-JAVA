978916157952bbb3561eb97ca4379f78455935da
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-25 11:39:38 +0530

    Time: 1 ms (99.62%), Space: 42.5 MB (92.58%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,16 +1,39 @@
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//        ArrayList<Integer> list = new ArrayList<>();
+//         for(int ele : stones){
+//             list.add(ele);
+//         }////O(n)
+
+//         while(list.size() > 1){
+//             Collections.sort(list);
+//             int y = list.remove(list.size() -1);
+//             int x = list.remove(list.size() -1);
+//             if(y != x){
+//                 y = y - x;
+//                 list.add(y);
+//             }
+//         } //// almost O(n2log(n));
+//         if(list.size() == 0) return 0;
+//         return list.remove(0);
+//     }
+// }////TC = O(n) + O(n2log(n));
+
+
+
 class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int ele : stones){
             pq.add(ele);
-        }//O(n)
+        }
         while(pq.size() > 1){
             int x = pq.remove();
             int y = pq.remove();
             if(x>y) pq.add(x-y);
             else if(x<y) pq.add(y-x);
-        } // almost O(n2log(n));
+        } 
         if(pq.size() == 0) return 0;
         return pq.remove();
     }
-}//TC = O(n) + O(n2log(n));
+}
