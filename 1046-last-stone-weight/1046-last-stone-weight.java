c671664f8d68921e5a7f8f8b9745c625c999523e
Saurabh Tiwari <saurabhedict@gmail.com>
2026-09-25 11:37:40 +0530

    Time: 1 ms (99.62%), Space: 42.7 MB (84.63%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -3,14 +3,14 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int ele : stones){
             pq.add(ele);
-        }
+        }//O(n)
         while(pq.size() > 1){
             int x = pq.remove();
             int y = pq.remove();
             if(x>y) pq.add(x-y);
             else if(x<y) pq.add(y-x);
-        }
+        } // almost O(n2log(n));
         if(pq.size() == 0) return 0;
         return pq.remove();
     }
-}
+}//TC = O(n) + O(n2log(n));
